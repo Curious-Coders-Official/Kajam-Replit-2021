3cd35dbd8ad89ba4c84d39c1ddb251ca505fd869
KrishAgarwal2811 <krishagarwal2811@gmail.com>
2021-10-20 13:38:08 +0530

    adding joystick

--- game/gameScene.js
+++ game/gameScene.js
@@ -6,10 +6,16 @@ class GameScene extends Phaser.Scene {
   }
   preload() {
     this.canvas = this.sys.game.canvas; //  Sets the canvas property for ease of acess
+
+    // joystick plugin hehe
+    /* this.load.plugin(
+      "joystickplugin",
+      "./joystick.min.js",
+      true
+    ); */
   }
+
   create() {
-
-
     // Player
     this.player = this.add.rectangle(300, 200, 50, 70, 0xff0000);
     this.physics.add.existing(this.player);
@@ -29,20 +35,49 @@ class GameScene extends Phaser.Scene {
 
     this.cursors = this.input.keyboard.createCursorKeys();
 
-
     // Camera
-    this.cameras.main.setBounds(0, 0, this.canvas.height * 2 + 3000, this.canvas.height * 2 + 200);
+    this.cameras.main.setBounds(
+      0,
+      0,
+      this.canvas.height * 2 + 3000,
+      this.canvas.height * 2 + 200
+    );
     this.cameras.main.startFollow(this.player);
-    this.cameras.main.setBackgroundColor('#ccccff');
+    this.cameras.main.setBackgroundColor("#ccccff");
+
+    // joystick
+    this.joysctick = this.plugins
+      .get("joystickplugin")
+      .add(this, {
+        radius: 50,
+        x: 30,
+        y: 30,
+        dir: 1,
+        base: this.add.circle(0, 0, 50, 0x888888),
+        thumb: this.add.circle(0, 0, 25, 0xef1313),
+      })
+      .on("update", this.handleJoystick, this);
+    this.print = this.add.text(0, 0).setText;
   }
+
   update() {
     if (this.cursors.left.isDown) {
       this.player.body.x = this.player.body.x - 10;
     } else if (this.cursors.right.isDown) {
       this.player.body.x = this.player.body.x + 10;
-    } if ((this.cursors.space.isDown || this.cursors.up.isDown) && this.player.body.onFloor()) {
+    }
+    if (
+      (this.cursors.space.isDown || this.cursors.up.isDown) &&
+      this.player.body.onFloor()
+    ) {
       this.player.body.setVelocityY(-900);
     }
+  }
+
+  handleJoystick() {
+    let cursorKeys = this.joysctick.createCursorKeys();
+    let keyDown = cursorKeys.find((key) => key.isDown);
+    this.print(keyDown);
   }
 }
 
--- game/index.js
+++ game/index.js
@@ -1,22 +1,30 @@
 const Phaser = require("phaser");
 const GameScene = require("./gameScene");
+const joystick = require('./joystick.min.js')
 
 let width = window.innerWidth * 2;
 let height = window.innerHeight * 2;
-
 
 var config = {
   type: Phaser.AUTO,
   width,
   height,
   physics: {
-    default: 'arcade',
+    default: "arcade",
     arcade: {
       gravity: { y: 1500 },
-      debug: true
-    }
+      debug: true,
+    },
   },
-  scene: GameScene
+  plugins: {
+        global: [{
+            key: 'joystick',
+            plugin: joystick,
+            start: true
+        },
+        ]
+    },
+  scene: GameScene,
 };
 
 var game = new Phaser.Game(config);
